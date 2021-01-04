893242850160a8464d3bff6dc47b1a547669b9d6
Afdhalul Ichsan Yourdan <40620980+ShennBoku@users.noreply.github.com>
2021-01-04 23:25:13 +0700

    Update AtlanticMutasi.php

--- AtlanticMutasi.php
+++ AtlanticMutasi.php
@@ -2,9 +2,9 @@
 /*
 .---------------------------------------------------------------------------.
 |    Script: Atlantic Mutasi                                                |
-|   Version: 1.4.1                                                          |
+|   Version: 1.5.6                                                          |
 |   Release: November 24, 2019 (12:27 WIB)                                  |
-|    Update: September 16, 2020 (13:02 WIB)                                 |
+|    Update: January 03, 2021 (15:27 WIB)                                   |
 |                                                                           |
 |                     Pasal 57 ayat (1) UU 28 Tahun 2014                    |
 |      Copyright © 2019, Afdhalul Ichsan Yourdan. All Rights Reserved.      |
@@ -20,24 +20,26 @@
 
 class AtlanticMutasi
 {
-    private $url = 'https://api.atlantic-group.co.id';
-    private $auth;
-    private $key;
+    private $url = 'https://atlantic-group.co.id/api/v1';
+    public $id;
+    public $key;
+    public $sid;
     
-    private $proxy_use;
+    public $proxy_use;
     private $proxy_url = 'proxy.rapidplex.com:3128'; // URL:Port
     private $proxy_auth = 'user:domainesia'; // Username:Password
 
     public function __construct($punten) {
-        $this->auth = $punten['auth'];
+        $this->id = $punten['id'];
         $this->key = $punten['key'];
+        $this->sid = $punten['sid'];
         $this->proxy_use = (isset($punten['proxy'])) ? $punten['proxy'] : false;
     }
 
     public function info($q = '') {
         $bank = strtoupper($q);
         if(in_array($bank,['BCA','BNI','GOPAY','OVO'])) {
-            return $this->connect($this->url.'/mutasi/info',['bank' => $bank]);
+            return $this->connect($this->url.'/mutasi/info',['payment' => $bank]);
         } else {
             return $this->connect($this->url.'/mutasi/info');
         }
@@ -55,15 +57,16 @@ class AtlanticMutasi
         return $this->connect($this->url.'/mutasi/gopay',['limit' => $limit,'quantity' => $qty,'description' => $desc]);
     }
 
-    public function gopayTrf($act,$phone,$amount,$secure,$description = '') {
-        if($act == 'detail') {
-            return $this->connect($this->url.'/transfer/gopay/detail',['target' => $phone]);
-        } else if($act == 'transfer') {
-            return $this->connect($this->url.'/transfer/gopay/',['target' => $phone,'jumlah' => $amount,'pin' => $secure,'description' => $description]);
-        } else {
-            return ['result' => false,'data' => null,'message' => 'Invaliid Action!'];
-        }
-    }
+    /* Tidak bisa digunakana lagi */
+    // public function gopayTrf($act,$phone,$amount,$secure,$description = '') {
+    //     if($act == 'detail') {
+    //         return $this->connect($this->url.'/transfer/gopay/detail',['target' => $phone]);
+    //     } else if($act == 'transfer') {
+    //         return $this->connect($this->url.'/transfer/gopay/',['target' => $phone,'jumlah' => $amount,'pin' => $secure,'description' => $description]);
+    //     } else {
+    //         return ['result' => false,'data' => null,'message' => 'Invaliid Action!'];
+    //     }
+    // }
 
     public function ovo($limit = 10,$qty = '',$desc = '') {
         return $this->connect($this->url.'/mutasi/ovo',['limit' => $limit,'quantity' => $qty,'description' => $desc]);
@@ -97,23 +100,27 @@ class AtlanticMutasi
 
     private function connect($end_point,$postdata = []) {
         $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, $end_point);
         if($this->proxy_use == true) curl_setopt($ch, CURLOPT_PROXY, $this->proxy_url);
         if($this->proxy_use == true) curl_setopt($ch, CURLOPT_PROXYUSERPWD, $this->proxy_auth);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-        curl_setopt($ch, CURLOPT_HTTPHEADER, ['Authentication: '.$this->auth]);
-        curl_setopt($ch, CURLOPT_POST, 1);
-        $postdata['api_key'] = $this->key;
-        curl_setopt($ch, CURLOPT_POSTFIELDS, $postdata);
-        $chresult = curl_exec($ch);
-        $json_result = json_decode($chresult, true);
-        return $json_result;
+        curl_setopt($ch, CURLOPT_HTTPHEADER, ['Content-Type: application/x-www-form-urlencoded']);
+        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        $postdata['key'] = $this->key;
+        $postdata['sid'] = $this->sid;
+        $postdata['sign'] = md5($this->id.$this->key);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($postdata));
+        curl_setopt($ch, CURLOPT_URL, $end_point);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+        $result = curl_exec($ch);
+        curl_close($ch);
+        return json_decode($result, true);
     }
 }
 
 $AtlMutasi = new AtlanticMutasi([
-    'auth'  => '', // API Auth
-    'key'   => '',  // API Key
+    'id'    => '', // API ID
+    'key'   => '', // API Key
+    'sid'   => '', // Subscription ID
     'proxy' => false // Proxy Connection
 ]);
